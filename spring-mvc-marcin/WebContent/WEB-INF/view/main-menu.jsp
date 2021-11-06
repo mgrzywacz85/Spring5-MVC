f6fe3ea0443eab39626427f4ac0b6249cfc7b47a
unknown <professor.marcin@gmail.com>
2021-11-06 22:02:57 +0100

    Server launches and home page displayed

---
+++ spring-mvc-marcin/WebContent/WEB-INF/view/main-menu.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+
+	<h2>Spring MVC Demo Marcin - Home Page</h2>
+
+</body>
+
+</html>
---
+++ spring-mvc-marcin/src/com/marcin/springmvc/HomeController.java
@@ -0,0 +1,14 @@
+package com.marcin.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String showPage() {
+		return "main-menu";
+	}
+
+}
